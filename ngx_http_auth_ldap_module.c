83a7cf972cc7a2e9b208ef2158d4cbddc313b6f0
Dmitry Klionsky <dm.klionsky@gmail.com>
2023-12-19 17:52:20 +0300

    Better return 503 error on auth timeout

--- ngx_http_auth_ldap_module.c
+++ ngx_http_auth_ldap_module.c
@@ -2526,7 +2526,9 @@ ngx_http_auth_ldap_authenticate(ngx_http_request_t *r, ngx_http_auth_ldap_ctx_t 
             ngx_queue_remove(&ctx->queue);
         }
 
-        return ngx_http_auth_ldap_set_realm(r, &conf->realm);
+        // Instead of questionable 401, better return 503
+        // ngx_http_auth_ldap_set_realm(r, &conf->realm);
+        return NGX_HTTP_SERVICE_UNAVAILABLE;
     }
 
     /*
